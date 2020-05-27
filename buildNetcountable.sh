1c76d4190dde55b77d6d340aaa5493049a881808
CamHart <camhart73@gmail.com>
2020-05-26 17:39:36 -0700

    adding eslint ignore

--- buildNetcountable.sh
+++ buildNetcountable.sh
@@ -9,6 +9,8 @@ npm install
 cd ..
 cd ..
 
+touch ../netcountable-e3kit/e3kit-base/.eslintignore
+
 rm -rf ../netcountable-e3kit/e3kit-browser
 mkdir ../netcountable-e3kit/e3kit-browser
 cp ./packages/e3kit-browser/browser.es.js ../netcountable-e3kit/e3kit-browser
@@ -18,6 +20,7 @@ cp ./packages/e3kit-browser/README.md ../netcountable-e3kit/e3kit-browser
 cp ./packages/e3kit-browser/worker.cjs.js ../netcountable-e3kit/e3kit-browser
 cp ./packages/e3kit-browser/worker.es.js ../netcountable-e3kit/e3kit-browser
 cp -r ./packages/e3kit-browser/dist ../netcountable-e3kit/e3kit-browser
+touch ../netcountable-e3kit/e3kit-browser/.eslintignore
 
 rm -rf ../netcountable-e3kit/e3kit-node
 mkdir ../netcountable-e3kit/e3kit-node
@@ -25,3 +28,4 @@ cp ./packages/e3kit-node/declaration.d.ts ../netcountable-e3kit/e3kit-node
 cp ./packages/e3kit-node/package.json ../netcountable-e3kit/e3kit-node
 cp ./packages/e3kit-node/README.md ../netcountable-e3kit/e3kit-node
 cp -r ./packages/e3kit-node/dist ../netcountable-e3kit/e3kit-node
+touch ../netcountable-e3kit/e3kit-node/.eslintignore
